6b78b010ac1f202085a36cd6550fe75a29e58baa
sc2bat <izar_dero@naver.com>
2024-01-11 08:55:03 +0900

    creating main view model

--- lib/ui/main/main_view_model.dart
+++ lib/ui/main/main_view_model.dart
@@ -5,13 +5,28 @@ import 'package:my_flutter_rate_app/ui/main/main_state.dart';
 class MainViewModel extends ChangeNotifier {
   final RateRepository _rateRepository;
 
-  final MainState _mainState = MainState();
+  MainState _mainState = MainState();
 
-  MainState get mainState = _mainState;
+  MainState get mainState => _mainState;
 
   MainViewModel({
     required RateRepository rateRepository,
-  }) : _rateRepository = rateRepository{
+  }) : _rateRepository = rateRepository {
     _updateRateResult(mainState.baseCode);
+  }
+  Future<void> _updateRateResult(String baseCode) async {
+    final result = await _rateRepository.getRateResult(baseCode);
+    _mainState = mainState.copyWith(rateResult: result);
+    notifyListeners();
+  }
+
+  void inputBaseCurrency(num baseCurrency) {
+    _mainState = mainState.copyWith(
+      baseCurrency: baseCurrency,
+      targetCurrency: baseCurrency *
+          mainState.rateResult!.rates
+              .firstWhere((element) => element.code == mainState.targetCode)
+              .rate,
+    );
   }
 }
